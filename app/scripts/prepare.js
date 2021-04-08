044c01ffe0c1c38b053d92a7f9a47c0d8f39baf1
Sean McClure <seanaaron100@gmail.com>
2021-04-08 17:12:56 +0000

    adding prepare.js

--- app/scripts/prepare.js
+++ app/scripts/prepare.js
@@ -1,23 +1,28 @@
 az.style_page({
-    "backgrond" : "#2f3640",
+    "background" : "#2f3640",
     "top" : 0,
     "left" : 0,
     "right" : 0,
-    "bottom" : 0
+    "bottom" : 0,
+    "margin" : "0px"
 })
 
-az.style_html({
-    "backgrond" : "#2f3640",
+az.style_body({
+    "background" : "#2f3640",
     "top" : 0,
     "left" : 0,
     "right" : 0,
-    "bottom" : 0
+    "bottom" : 0,
+    "margin" : "0px"
 })
 
 az.add_sections({
-    "this_class" : "main_section"
+    "this_class" : "main_section",
+    "sections" : 1
 })
 
 az.style_sections("main_section", 1, {
-    "background" : "#2f3640"
+    "background" : "#353b48",
+    "width" : "100vw"
 })
+
